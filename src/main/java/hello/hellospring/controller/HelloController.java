506f60367284e0b9dee868c9f2ae9afb6372964e
9shhh <uelj1234@naver.com>
2022-08-10 15:03:27 +0900

    스프링 mvc 테스트 - view, controller 영역에 대한 분리 - @ResponseBody 어노테이션을 통한 http body 전달 - @ResponseBody 어노테이션을 만났을 때 json 또는 다른 여러 형태로 convert 되는 것에 이해

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,5 +16,40 @@ public class HelloController {
 
         // 스프링 부트에서 resources/templates의 hello.html을 찾아서 렌더링 함.(Thymeleaf 템플릿 엔진 처리 포함)
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 여기서 ResponseBody의 의미는 html의 body를 의미하는 것이 아니라, http의 header와 body 부의 body를 의미함. 이 body의 데이터를 직접 정의 및 제공하겠다는 의미.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // json 방식으로 리턴됨.
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        // getter, setter
+        // java bean 표준 방식, property 접근 방식 으로 불리우는 형태
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
