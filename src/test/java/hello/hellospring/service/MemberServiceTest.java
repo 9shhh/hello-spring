32210b378bdd5706623a0383d1536a5dc1ad1f5a
9shhh <uelj1234@naver.com>
2022-08-23 00:30:41 +0900

    회원 서비스 테스트 로직 수정

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -51,19 +51,18 @@ class MemberServiceTest {
 
         // when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
+        // then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-        // 이 방법 말고 위에 assertThrows 를 활용하자
+// 이 방법 말고 위에 assertThrows 를 활용하자
 //        try {
 //            memberService.join(member2);
 //            fail();
 //        } catch (IllegalStateException e) {
 //            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 //        }
-
-        // then
     }
 
     @Test
