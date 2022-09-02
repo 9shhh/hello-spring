2325fe977086896b098bc5a481055cd412a3321c
9shhh <uelj1234@naver.com>
2022-09-02 15:56:19 +0900

    jpa 적용 및 실습 - gradle 추가 (jpa) - application.properties 추가 (jpa) - JpaMemberRepository 생성 - Member domain class @Entity 정의 - springConfig 수정 (jpa EntityManager 기반)

---
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,47 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class JpaMemberRepository implements MemberRepository {
+
+    private EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member); // persist : 영속하다, 영구 저장 하다.
+        return member;
+        // 이렇게만 작성하면 jpa가 insert query 만들고 db에 질의하고, member에 setId 까지 함.
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // pk 기반이 아닌 조회 작업은 jpql을 통해 질의하여 데이터를 조회한다.
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
